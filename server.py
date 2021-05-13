1c99720a78d0b14d9bd2a4b9913fcad575c68373
Sergiy <44747525+Tsurkanovskiy@users.noreply.github.com>
2021-05-13 18:59:23 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request, redirect
 from persistance import persistance_read, persistance_create, persistance_update, persistance_delete, app, create_tables
+import os
 
+
+
+app = app
 create_tables()
 
 @app.route("/")
 def main():
 	all_matches = persistance_read()
-	return render_template("main.html", all_matches=all_matches)
+	DB_URL1 = os.environ.get("DATABASE_URL")
+	return render_template("main.html", all_matches=all_matches, DB_URL = DB_URL1)
 
 @app.route("/create", methods=["POST"])
 def addNewTitle():
